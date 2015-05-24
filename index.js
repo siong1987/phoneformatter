208f734da670558e3486aa5dabd86100da2cef19
Siong <siong1987@gmail.com>
2015-05-23 18:39:00 -0700

    post/get request.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+var phoneNumberFormat = require('google-libphonenumber').PhoneNumberFormat;
+
 var express = require('express');
-var libphonenumber = require('google-libphonenumber');
+var bodyParser = require('body-parser');
 
 var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
 var oneDay = 86400000;
 app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
@@ -12,6 +15,14 @@ app.set('port', (process.env.PORT || 5000))
 
 app.get('/', function(request, response) {
   response.render('main');
+});
+
+app.post('/phone', function(request, response) {
+  response.send('phone ' + request.body.number);
+});
+
+app.get('/phone/:number', function(request, response) {
+  response.send('phone ' + request.params.number);
 });
 
 app.listen(app.get('port'), function() {
